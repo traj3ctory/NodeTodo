df3b41599a81b51016a938f5d855597334cc2eb5
davebenard <benard.zibiri@e4email.net>
2021-11-01 13:42:00 +0100

    feat: progress

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,41 +1,45 @@
 module.exports = (app) => {
+  const MongoClient = require("mongodb").MongoClient;
+  const password = "CharMZ06";
+  // const uri = "mongodb+srv://traj3ctory:CharMZ06@cluster0.ic2kd.mongodb.net/traj3ctory?retryWrites=true&w=majority";
+  const uri = `mongodb+srv://traj3ctory:${password}@cluster0.ic2kd.mongodb.net/traj3ctory?retryWrites=true&w=majority`;
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  let bodyParser = require("body-parser");
 
-    const MongoClient = require('mongodb').MongoClient;
-    const password = 'CharMZ06';
-    // const uri = "mongodb+srv://traj3ctory:CharMZ06@cluster0.ic2kd.mongodb.net/traj3ctory?retryWrites=true&w=majority";
-    const uri = `mongodb+srv://traj3ctory:${password}@cluster0.ic2kd.mongodb.net/traj3ctory?retryWrites=true&w=majority`
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    let bodyParser = require('body-parser');
+  let urlencodedParser = bodyParser.urlencoded({
+    extended: false,
+  });
 
-    let urlencodedParser = bodyParser.urlencoded({
-        extended: false
+  client.connect((err) => {
+    const db = client.db("todo");
+    const Todo = db.collection("task");
+
+    app.get("/todo", (req, res) => {
+      //get data from mongodb and display res
+      Todo.find({}).toArray((err, data) => {
+        if (err) throw err;
+        res.render("todo", { todos: data });
+      });
     });
 
-    client.connect(err => {
-        const db = client.db("todo");
-        const Todo = db.collection("todos");
+    app.post("/todo", urlencodedParser, (req, res) => {
+      Todo.insertOne(req.body, (err, data) => {
+        if (err) throw err;
+        res.json(data);
+      });
+    });
 
-        app.get('/todo', (req, res) => {
-            //get data from mongodb and display res
-            Todo.find({}).toArray((err, data) => {
-                if (err) throw err;
-                res.render('todo', { todos: data });
-            })
-        });
-
-        app.post('/todo', urlencodedParser, (req, res) => {
-            Todo.insertOne((req.body), (err, data) => {
-                if (err) throw err;
-                res.json(data);
-            })
-        });
-
-        app.delete('/todo/:item', (req, res) => {
-            Todo.deleteOne({ item: req.params.item.replace(/\-/g, " ") }, (err, data) => {
-                if (err) throw (err);
-                console.log(err)
-                res.json(data);
-            })
-        });
-    })
+    app.delete("/todo/:item", (req, res) => {
+      Todo.deleteOne(
+        { item: req.params.item.replace(/\-/g, "") },
+        (err, data) => {
+          if (err) throw err;
+          res.json(data);
+        }
+      );
+    });
+  });
 };
