641aaefcdc250cc17167878a36dd0faaf1d97865
traj3ctory <davidbenard.bd@gmail.com>
2022-01-16 13:55:45 +0100

    prog

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,43 +1,41 @@
 module.exports = (app) => {
 
-    const MongoClient = require('mongodb').MongoClient;
-    const password = 'applocation';
-    // const uri = "mongodb+srv://traj3ctory:applocation@cluster0.ic2kd.mongodb.net/traj3ctory?retryWrites=true&w=majority";
-    const uri = `mongodb+srv://traj3ctory:${password}@cluster0.ic2kd.mongodb.net/traj3ctory?retryWrites=true&w=majority`
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    const bodyParser = require('body-parser');
+  const MongoClient = require('mongodb').MongoClient;
+  const password = 'applocation';
+  // const uri = "mongodb+srv://traj3ctory:CharMZ06@cluster0.ic2kd.mongodb.net/traj3ctory?retryWrites=true&w=majority";
+  const uri = `mongodb+srv://traj3ctory:${password}@cluster0.ic2kd.mongodb.net/traj3ctory?retryWrites=true&w=majority`
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  let bodyParser = require('body-parser');
 
-    const urlencodedParser = bodyParser.urlencoded({
-        extended: false
-    });
+  let urlencodedParser = bodyParser.urlencoded({
+      extended: false
+  });
 
-    client.connect(err => {
-        const db = client.db("todo");
-        const Todo = db.collection("todos");
+  client.connect(err => {
+      const db = client.db("todo");
+      const Todo = db.collection("todos");
 
-        app.get('/todo', (req, res) => {
-            //get data from mongodb and display res
-            Todo.find({}).toArray((err, data) => {
-                if (err) throw err;
-                res.render('todo', { todos: data });
-            })
-        });
+      app.get('/todo', (req, res) => {
+          //get data from mongodb and display res
+          Todo.find({}).toArray((err, data) => {
+              if (err) throw err;
+              res.render('todo', { todos: data });
+          })
+      });
 
-        app.post('/todo', urlencodedParser, (req, res) => {
-            Todo.insertOne((req.body), (err, data) => {
-                if (err) throw err;
-                res.json(data);
-            })
-        });
+      app.post('/todo', urlencodedParser, (req, res) => {
+          Todo.insertOne((req.body), (err, data) => {
+              if (err) throw err;
+              res.json(data);
+          })
+      });
 
-        app.delete('/todo/:item', (req, res) => {
-          const id = req.params.item.replace(/\-/g, "");
-            Todo.deleteOne({ item: id }, (err, data) => {
-                if (err) throw (err);
-                
-                console.log(data);
-                res.json(data);
-            })
-        });
-    })
+      app.delete('/todo/:item', (req, res) => {
+
+          Todo.deleteOne({ item: req.params.item.replace(/\-/g, " ") }, (err, data) => {
+              if (err) throw (err);
+              res.json(data);
+          })
+      });
+  })
 };
--- public/asset/todo.js
+++ public/asset/todo.js
@@ -1,30 +1,34 @@
-$(document).ready(() => {
-  $("form").on("submit", () => {
-    const item = $("form input");
-    const todo = { item: item.val() };
+$(document).ready(function(){
 
-    $.ajax({
-      type: "POST",
-      url: "/todo",
-      data: todo,
-      success(data) {
-        //do something with the data via front-end framework
-        location.reload();
-      },
-    });
+  $('form').on('submit', function(){
 
-    return false;
+      var item = $('form input');
+      var todo = {item: item.val()};
+
+      $.ajax({
+        type: 'POST',
+        url: '/todo',
+        data: todo,
+        success: function(data){
+          //do something with the data via front-end framework
+          location.reload();
+        }
+      });
+
+      return false;
+
   });
 
-  $("li").on("click", function () {
-    const item = $(this).text().replace(/ /g, "-");
-    $.ajax({
-      type: "DELETE",
-      url: `/todo/${item}`,
-      success(data) {
-        //do something with the data via front-end framework
-        location.reload();
-      },
-    });
+  $('li').on('click', function(){
+      var item = $(this).text().replace(/ /g, "-");
+      $.ajax({
+        type: 'DELETE',
+        url: '/todo/' + item,
+        success: function(data){
+          //do something with the data via front-end framework
+          location.reload();
+        }
+      });
   });
+
 });
